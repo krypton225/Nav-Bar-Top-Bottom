ff4b173d7748b8827c924d6d08d75ccfcccd44fb
Krypton <k.mohamed.11.98@gmail.com>
2022-01-18 20:07:40 +0200

    ADD files

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// function printSum(x = 2, y = 1) {
+//   return x + y;
+// }
+
+// let myName = "Khaled Mohamed";
+
+// console.log(myName.includes("led"));
+
+function toggleMenu() {
+  var toggle = document.querySelector(".toggle");
+  toggle.classList.toggle("active");
+
+  var menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+}
